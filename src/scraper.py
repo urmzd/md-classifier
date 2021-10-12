1193af47c7a35a4c7c146f564cf4ca8a7c33c5f7
Urmzd <urmzds@gmail.com>
2021-10-11 22:16:42 -0400

    Add methods to extract data from websites

--- src/scraper.py
+++ src/scraper.py
@@ -1,8 +1,26 @@
-from typing import List
+from typing import List, Optional
+from re import search
+from requests import get
 
-def get_links_by_file_ref(file_ref:str="../resources/sources/headache.txt") -> List[str]:
+def get_links_by_file_ref(file_ref: str = "../resources/sources/headache.txt") -> List[str]:
     with open(file_ref, "r") as source_file:
         return [link_ref for link_ref in source_file]
+
+def get_html_by_link_ref(link_ref: str) -> Optional[str]:
+    response = get(link_ref)
+
+    if response.status_code == 200:
+        return response.text
+
+def get_body_from_html(html: str) -> Optional[str]:
+    body_regex = r"<body>(.*?)<\/body>"
+    matches = search(html, body_regex)
+
+    if matches:
+        return matches.group(0)
+
+
+
 
 
 
