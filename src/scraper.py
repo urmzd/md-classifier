16a7d739cf754a09098bd965bbdb9ac3a0a3f419
Urmzd <urmzds@gmail.com>
2021-10-12 22:22:08 -0400

    Provide the ability to scrape all main content
    Add a selector option to source text to ensure unneccessary data is not
    pulled.
    
    Add methods to extract text from the main content using the provided
    selector.

--- src/scraper.py
+++ src/scraper.py
@@ -1,27 +1,47 @@
-from typing import List, Optional
-from re import search
-from requests import get
+#!/usr/bin/env python
+# Create types.
+from typing import Optional, Tuple, List
+from bs4 import BeautifulSoup
+from pandas.core.frame import DataFrame
+import requests
+import pandas as pd
 
-def get_links_by_file_ref(file_ref: str = "../resources/sources/headache.txt") -> List[str]:
-    with open(file_ref, "r") as source_file:
-        return [link_ref for link_ref in source_file]
-
-def get_html_by_link_ref(link_ref: str) -> Optional[str]:
-    response = get(link_ref)
-
-    if response.status_code == 200:
-        return response.text
-
-def get_body_from_html(html: str) -> Optional[str]:
-    body_regex = r"<body>(.*?)<\/body>"
-    matches = search(html, body_regex)
-
-    if matches:
-        return matches.group(0)
+Link = str
+Selector = str
+LinkSelectorPair = Tuple[Link, Selector]
 
 
+def get_link_selector_pair_from_file_ref(
+    file_ref: str = "../resources/sources/headache.txt",
+) -> Optional[DataFrame]:
+    df = pd.read_csv(file_ref, index_col=False)
+
+    if isinstance(df, DataFrame):
+        # Strip white space from column data.
+        df.columns = df.columns.str.strip()
+        return df
 
 
+def get_html_text_from_link_and_selector(link: str, selector: str) -> List[str]:
+    html_main = BeautifulSoup(requests.get(link).text).select_one(selector)
+
+    if html_main:
+        return [html_text.get_text() for html_text in html_main.find_all(text=True)]
+
+    return []
 
 
+def get_html_text_from_data_frame(df: DataFrame):
+    return [
+        body_text
+        for r_i in range(len(df))
+        for body_text in get_html_text_from_link_and_selector(
+            df.iloc[r_i, 1], df.iloc[r_i, 0]
+        )
+    ]
 
+
+df = get_link_selector_pair_from_file_ref()
+
+if isinstance(df, DataFrame):
+   print(get_html_text_from_data_frame(df))
