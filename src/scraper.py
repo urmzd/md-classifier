1ab77210d270599cc2b65c4bd55a48aaf634883e
Urmzd <urmzds@gmail.com>
2021-10-12 22:33:21 -0400

    Add functions to clean up html text

--- src/scraper.py
+++ src/scraper.py
@@ -40,8 +40,15 @@ def get_html_text_from_data_frame(df: DataFrame):
         )
     ]
 
+def remove_all_white_space_from_texts(texts: List[str]):
+    return ",".join([subtext for text in texts for subtext in text.split(" ")])
 
-df = get_link_selector_pair_from_file_ref()
+def output_text_to_file_ref(text:str, file_ref:str="../resources/targets/headache.txt"):
+    with open(file_ref, "w") as target:
+        target.write(text)
 
-if isinstance(df, DataFrame):
-   print(get_html_text_from_data_frame(df))
+if __name__ == "__main__":
+    df = get_link_selector_pair_from_file_ref()
+
+    if isinstance(df, DataFrame):
+       print(get_html_text_from_data_frame(df))
