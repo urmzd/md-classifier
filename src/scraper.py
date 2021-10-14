576694f02d1a66d04f8599eddeb8c2422c2d83e5
Urmzd <urmzds@gmail.com>
2021-10-13 21:03:37 -0400

    Correction - ensure csv is provided and outputted
    Rename the file extension for pandas and associated functions.
    
    By renaming the extension, pandas correctly parses the csv file,
    allow for the data frame to populated with the true values instead of
    random NaN's.
    
    Adjusted the associated functions to handle ".csv" instead of ".txt".

--- src/scraper.py
+++ src/scraper.py
@@ -11,19 +11,24 @@ Selector = str
 LinkSelectorPair = Tuple[Link, Selector]
 
 
-def get_link_selector_pair_from_file_ref(
-    file_ref: str = "../resources/sources/headache.txt",
+def get_df_from_csv(
+    file_ref: str = "../resources/sources/headache.csv",
 ) -> Optional[DataFrame]:
-    df = pd.read_csv(file_ref, index_col=False)
+    df = pd.read_csv(file_ref)
 
     if isinstance(df, DataFrame):
         # Strip white space from column data.
+        print(df)
         df.columns = df.columns.str.strip()
         return df
 
+    return None
 
-def get_html_text_from_link_and_selector(link: str, selector: str) -> List[str]:
-    html_main = BeautifulSoup(requests.get(link).text).select_one(selector)
+
+def get_html(link: str, selector: str) -> List[str]:
+    html_main = BeautifulSoup(
+        requests.get(link).text, features="html.parser"
+    ).select_one(selector)
 
     if html_main:
         return [html_text.get_text() for html_text in html_main.find_all(text=True)]
@@ -31,24 +36,41 @@ def get_html_text_from_link_and_selector(link: str, selector: str) -> List[str]:
     return []
 
 
-def get_html_text_from_data_frame(df: DataFrame):
-    return [
-        body_text
-        for r_i in range(len(df))
-        for body_text in get_html_text_from_link_and_selector(
-            df.iloc[r_i, 1], df.iloc[r_i, 0]
-        )
-    ]
+def get_html_from_df(df: Optional[DataFrame]):
+    if df is not None:
+        return [
+            body_text
+            for r_i in range(len(df))
+            for body_text in get_html(df.iloc[r_i, 1], df.iloc[r_i, 0])
+        ]
+    return []
 
-def remove_all_white_space_from_texts(texts: List[str]):
-    return ",".join([subtext for text in texts for subtext in text.split(" ")])
 
-def output_text_to_file_ref(text:str, file_ref:str="../resources/targets/headache.txt"):
+def split_words_by_whitespace(texts: List[str]):
+    return [subtext for text in texts for subtext in text.split(" ")]
+
+
+def create_csv_string(texts: List[str]) -> str:
+    return ",".join(texts)
+
+
+def output_text_to_file_ref(
+    text: str, file_ref: str = "../resources/targets/headache.csv"
+):
     with open(file_ref, "w") as target:
         target.write(text)
 
-if __name__ == "__main__":
-    df = get_link_selector_pair_from_file_ref()
 
-    if isinstance(df, DataFrame):
-       print(get_html_text_from_data_frame(df))
+if __name__ == "__main__":
+    df = get_df_from_csv()
+
+    if df is not None:
+        try:
+            output_text_to_file_ref(
+                create_csv_string(
+                    split_words_by_whitespace(get_html_from_df(get_df_from_csv()))
+                )
+            )
+            print("Parsing successful.")
+        except:
+            print("Parsing failed.")
